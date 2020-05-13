c27a1bfc5fcd2d6d9cc2d9ba7edd4fd861176458
Nei Freitas <neijfreitas@gmail.com>
2020-05-12 21:15:05 -0300

    Commit

--- cap03/nivel3/pessoa_list.php
+++ cap03/nivel3/pessoa_list.php
@@ -1,69 +1,27 @@
-<html>
 
-<head>
-    <meta charset="utf-8">
-    <title>Listagem de pessoas</title>
-    <link a href="css/list.css" rel="stylesheet" type="text/css" media="screen" />
-</head>
+<?php
+$conn = mysqli_connect('localhost', 'root', '', 'livro');
 
-<body>
-    <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'livro');
-    
-    if(!empty($_GET['action']) AND $_GET['action'] == 'delete'){
-        $id = (int) $_GET['id'];
-        $result = mysqli_query($conn, "DELETE FROM pessoa WHERE id='{$id}'");
-    }
-    
-    $result = mysqli_query($conn, "SELECT * FROM pessoa ORDER BY id");
+if(!empty($_GET['action']) AND $_GET['action'] == 'delete'){
+    $id = (int) $_GET['id'];
+    $result = mysqli_query($conn, "DELETE FROM pessoa WHERE id='{$id}'");
+}
 
-    // Monta o cabeçalho da tabela
-    print '<table border=1>';
-    print '<thead>';
-    print '<tr>';
-    print "<th> </th>";
-    print "<th> </th>";
-    print "<th> Id </th>";
-    print "<th> Nome </th>";
-    print "<th> Endereço </th>";
-    print "<th> Bairro </th>";
-    print "<th> Telefone </th>";
-    print "<th> Email </th>";
-    print '</tr>';
-    print '</thead>';
-    print '<tbody>';
-    while($row = mysqli_fetch_assoc($result)){
-        $id = $row['id'];
-        $nome = $row['nome'];
-        $endereco = $row['endereco'];
-        $bairro = $row['bairro'];
-        $telefone = $row['telefone'];
-        $email = $row['email'];
-        
-        print '<tr>';
-        print "<td align='center'>
-                <a href='pessoa_form.php?action=edit&id={$id}'>
-                <img src='images/edit.svg' style='width:17px'>
-                </a></td>";
-        print "<td align='center'>
-                <a href='pessoa_list.php?action=delete&id={$id}'>
-                <img src='images/remove.svg' style='width:17px'>
-                </a></td>";
-        print "<td>{$id}</td>";
-        print "<td>{$nome}</td>";
-        print "<td>{$endereco}</td>";
-        print "<td>{$bairro}</td>";
-        print "<td>{$telefone}</td>";
-        print "<td>{$email}</td>";
-        print '</tr>';
-    }
-    print '</tbody>';
-    print '</table>';
-    ?>
+$result = mysqli_query($conn, "SELECT * FROM pessoa ORDER BY id");
+$items = '';
 
-    <button onclick="window.location='pessoa_form.php'">
-        <img src='images/insert.svg' style='width:17px'> Inserir
-    </button>
-</body>
+while($row = mysqli_fetch_assoc($result)){
+    $item = file_get_contents('html/item.html');
+    $item = str_replace('{id}', $row['id'], $item);
+    $item = str_replace('{nome}', $row['nome'], $item);
+    $item = str_replace('{endereco}', $row['endereco'], $item);
+    $item = str_replace('{bairro}', $row['bairro'], $item);
+    $item = str_replace('{telefone}', $row['telefone'], $item);
+    $item = str_replace('{email}', $row['email'], $item);
+    $items .= $item;
+}
 
-</html>
+    $list = file_get_contents('html/list.html');
+    $list = str_replace('{items}', $items, $list);
+    print $list;
+?>
